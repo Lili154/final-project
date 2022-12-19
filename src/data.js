0baf7963bc2c89f2271d39bbe45b4145c1d82184
Lili154 <petrosyanlilit185@gmail.com>
2022-12-19 14:05:00 +0400

    'now'

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://cdn.shopify.com/s/files/1/2180/5147/files/slideshow_1.jpg?v=1631257803",
+      title: "BABY CLOTHES",
+      desc: "Babies keep moving...crawl,sit,climb",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://cdn.shopify.com/s/files/1/2180/5147/files/b5_1053215f-eed0-4d2b-96c0-d3c637dec278.jpg?v=1613699295",
+      title: "TOYS",
+      desc: "Babies keep moving...crawl,sit,climb",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://cdn.shopify.com/s/files/1/2180/5147/files/b4_large.jpg?v=1613698961",
+      title: "MILK BOTTLES",
+      desc: "Babies keep moving...crawl,sit,climb",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://cdn.shopify.com/s/files/1/2180/5147/files/slideshow_1.jpg?v=1631257803",
+      title: "BABY CLOTHES",
+    },
+    {
+      id: 2,
+      img: "https://cdn.shopify.com/s/files/1/2180/5147/files/b5_1053215f-eed0-4d2b-96c0-d3c637dec278.jpg?v=1613699295",
+      title: "TOYS",
+    },
+    {
+      id: 3,
+      img: "https://cdn.shopify.com/s/files/1/2180/5147/files/b4_large.jpg?v=1613698961",
+      title: "MILK BOTTLES",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://cdn.shopify.com/s/files/1/2180/5147/products/baby20_540x.jpg?v=1500104417",
+    },
+    {
+      id:2,
+      img:"https://cdn.shopify.com/s/files/1/2180/5147/products/baby28_2000x.jpg?v=1500104556",
+    },
+    {
+      id:3,
+      img:"https://cdn.shopify.com/s/files/1/2180/5147/products/baby31_600x.jpg?v=1500104604",
+    },
+    {
+      id:4,
+      img:"https://cdn.shopify.com/s/files/1/2180/5147/products/baby14_600x.jpg?v=1500104297",
+    },
+    {
+      id:5,
+      img:"https://cdn.shopify.com/s/files/1/2180/5147/products/baby36_600x.jpg?v=1500104766",
+    },
+    {
+      id:6,
+      img:"https://cdn.shopify.com/s/files/1/2180/5147/products/baby24_600x.jpg?v=1500104482",
+    },
+    {
+      id:7,
+      img:"https://cdn.shopify.com/s/files/1/2180/5147/products/baby10_600x.jpg?v=1500103611",
+    },
+    {
+      id:8,
+      img:"https://cdn.shopify.com/s/files/1/2180/5147/products/baby7_2000x.jpg?v=1612951901",
+    },
+  ]
